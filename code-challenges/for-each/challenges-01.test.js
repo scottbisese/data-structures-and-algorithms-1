a1bb575d81bfa9082231055f4bec994394a7786a
francescodt <francescodtroiano@gmail.com>
2020-03-31 12:21:06 -0500

    fixed 2 so its a for loop

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -37,16 +37,12 @@ const addValues = (arr, value) => {
 };
 
 const addNumbers = (num, arr, times, callback) => {
-  let count = 0;
-
-  while (count < times) {
-    callback(arr,num);
-
-    count++;
-
+  for (let i = 0; i < times; i++) {
+    callback(arr, num);
   }
   return arr;
 };
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
